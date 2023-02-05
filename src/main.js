8b4d7a2f52a83a505605c57ff29caa8269e66440
bemxio <bemxiov@protonmail.com>
2023-02-05 15:48:15 +0100

    fix issues and implement the image drawing

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ const { program } = require("commander");
 // functions
 const colorImage = (image, color) => {
     // convert image to canvas
-    const canvas = document.createElement("canvas");
+    const canvas = createCanvas(image.width, image.height);
     const ctx = canvas.getContext("2d");
-
-    canvas.width = image.width;
-    canvas.height = image.height;
 
     ctx.drawImage(image, 0, 0);
     
@@ -40,14 +37,55 @@ program.option("-b, --background [color]", "The color of the background, can be 
 program.option("-c, --color [color]", "The color of the pattern, can be any valid CSS value.", "#5e5e5e");
 program.option("-r, --rotation [degrees]", "The rotation of the image pattern, in degrees.", 45);
 
+program.option("-x, --offset-x [pixels]", "The offset of the pattern on the X axis, in pixels.", 0);
+program.option("-y, --offset-y [pixels]", "The offset of the pattern on the Y axis, in pixels.", 0);
+
 program.parse();
 
 const args = program.opts();
 
 // retrieving values from arguments
-const image = await loadImage(args.image);
+let image = loadImage(args.image);
+const outputPath = args.output;
 
 const backgroundColor = Color(args.background);
 const imageColor = Color(args.color);
-
 const rotation = args.rotation;
+
+const offsetX = args.offsetX;
+const offsetY = args.offsetY;
+
+// main code //
+const canvas = createCanvas(image.width, image.height);
+const ctx = canvas.getContext("2d");
+
+// fill background
+ctx.fillStyle = backgroundColor.string();
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// get camvas dimensions in rows and columns
+const width = image.width + offsetX;
+const height = image.height + offsetY;
+
+const rows = Math.floor(canvas.width / width);
+const columns = Math.floor(canvas.height / height);
+
+// change color of image
+image = colorImage(image, imageColor);
+
+// draw image
+for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < columns; y++) {
+        ctx.save();
+
+        console.log("X: " + (x * width) + ", Y: " + (y * height));
+        console.log("Row: " + x + ", Column: " + y);
+
+        ctx.translate(x * width, y * height);
+        ctx.rotate((rotation * Math.PI) / 180);
+
+        ctx.drawImage(image, 0, 0);
+
+        ctx.restore();
+    }
+}
